300cd3a0f2e199c5ccc238f13fa9ca7e2f0e436c
Shane da Silva <shane@dasilva.io>
2014-09-06 19:53:23 -0700

    Use module_function instead of class << self
    The `Utils` module had an unnecessary extra level of indention due to
    its use of `class << self` to make its helpers all class-level
    functions.
    
    Fix this by using the `module_function` helper, which accomplishes the
    same thing.
    
    Change-Id: I6194f394e111a6e7dac2672189472eb53ae62f04
    Reviewed-on: http://gerrit.causes.com/42352
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- lib/haml_lint/utils.rb
+++ lib/haml_lint/utils.rb
@@ -3,33 +3,33 @@ require 'find'
 module HamlLint
   # A miscellaneous set of utility functions.
   module Utils
-    class << self
-      def extract_files_from(list)
-        files = []
+    module_function
 
-        list.each do |file|
-          begin
-            Find.find(file) do |f|
-              files << f if haml_file?(f)
-            end
-          rescue Errno::ENOENT
-            # One of the paths specified does not exist; raise a more
-            # descriptive exception so we know which one
-            raise HamlLint::Exceptions::InvalidFilePath,
-                  "File path '#{file}' does not exist"
+    def extract_files_from(list)
+      files = []
+
+      list.each do |file|
+        begin
+          Find.find(file) do |f|
+            files << f if haml_file?(f)
           end
+        rescue Errno::ENOENT
+          # One of the paths specified does not exist; raise a more
+          # descriptive exception so we know which one
+          raise HamlLint::Exceptions::InvalidFilePath,
+                "File path '#{file}' does not exist"
         end
-
-        files.uniq
       end
 
-    private
+      files.uniq
+    end
 
-      def haml_file?(file)
-        return false unless FileTest.file?(file)
+  private
 
-        File.extname(file) == '.haml'
-      end
+    def haml_file?(file)
+      return false unless FileTest.file?(file)
+
+      File.extname(file) == '.haml'
     end
   end
 end
