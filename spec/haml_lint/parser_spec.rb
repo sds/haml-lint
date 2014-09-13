c325c5a3af58ab0e11d838cef840ba715a862c11
Shane da Silva <shane@dasilva.io>
2014-09-12 22:18:59 -0700

    Indent heredocs in Parser spec
    Heredocs that aren't indented are more difficult to read. Fix that by
    indenting them and using the `normalize_indent` helper to avoid a parse
    error.
    
    Change-Id: Ia8cde89109290a548f421ad1db642e9c7926c7a4
    Reviewed-on: http://gerrit.causes.com/42552
    Tested-by: jenkins <jenkins@causes.com>
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>

--- spec/haml_lint/parser_spec.rb
+++ spec/haml_lint/parser_spec.rb
@@ -4,54 +4,54 @@ describe HamlLint::Parser do
   context 'when skip_frontmatter is true' do
     let(:parser) { HamlLint::Parser.new(haml, 'skip_frontmatter' => true) }
 
-    let(:haml) { <<-HAML }
----
-:key: value
----
-%tag
-  Some non-inline text
-- 'some code'
+    let(:haml) { normalize_indent(<<-HAML) }
+      ---
+      :key: value
+      ---
+      %tag
+        Some non-inline text
+      - 'some code'
     HAML
 
     it 'excludes the frontmatter' do
-      expect(parser.contents).to eq(<<-CONTENT)
-%tag
-  Some non-inline text
-- 'some code'
+      expect(parser.contents).to eq(normalize_indent(<<-CONTENT))
+        %tag
+          Some non-inline text
+        - 'some code'
       CONTENT
     end
 
     context 'when haml has --- as content' do
-      let(:haml) { <<-HAML }
----
-:key: value
----
-%tag
-  Some non-inline text
-- 'some code'
-  ---
-    HAML
+      let(:haml) { normalize_indent(<<-HAML) }
+        ---
+        :key: value
+        ---
+        %tag
+          Some non-inline text
+        - 'some code'
+          ---
+      HAML
 
       it 'is not greedy' do
-        expect(parser.contents).to eq(<<-CONTENT)
-%tag
-  Some non-inline text
-- 'some code'
-  ---
-      CONTENT
+        expect(parser.contents).to eq(normalize_indent(<<-CONTENT))
+          %tag
+            Some non-inline text
+          - 'some code'
+            ---
+        CONTENT
       end
     end
   end
 
   context 'when skip_frontmatter is false' do
     let(:parser) { HamlLint::Parser.new(haml, 'skip_frontmatter' => false) }
-    let(:haml) { <<-HAML }
----
-:key: value
----
-%tag
-  Some non-inline text
-- 'some code'
+    let(:haml) { normalize_indent(<<-HAML) }
+      ---
+      :key: value
+      ---
+      %tag
+        Some non-inline text
+      - 'some code'
     HAML
 
     it 'raises HAML error' do
