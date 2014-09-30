5b5347db1a3e146c6b01e2233ccda6b5913e56b7
Nicholas Chmielewski <hackling@gmail.com>
2014-09-30 16:40:49 +1000

    Add new specs to cover ImplicitDiv edge cases

--- spec/haml_lint/linter/implicit_div_spec.rb
+++ spec/haml_lint/linter/implicit_div_spec.rb
@@ -52,4 +52,20 @@ describe HamlLint::Linter::ImplicitDiv do
 
     it { should report_lint line: 3 }
   end
+
+  context 'when a div has ruby code inside curly braces' do
+    let(:haml) { <<-HAML }
+      %div{id: Object.to_s}
+    HAML
+
+    it { should_not report_lint }
+  end
+
+  context 'when div has ruby code inside square braces' do
+    let(:haml) { <<-HAML }
+      %div[Object.to_s]
+    HAML
+
+    it { should_not report_lint }
+  end
 end
