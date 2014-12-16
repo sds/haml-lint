46d7bb25fad5b05e669b7288656b222b50a0e145
Shane da Silva <shane.dasilva@brigade.com>
2014-12-16 05:12:23 +0000

    Fix documentation for Reporter class
    Change-Id: I324caa5900491528ae26cd38b3f382e0ca9b0c28
    Reviewed-on: http://gerrit.causes.com/45311
    Reviewed-by: Shane da Silva <shane.dasilva@brigade.com>
    Tested-by: jenkins <jenkins@brigade.com>

--- lib/haml_lint/reporter.rb
+++ lib/haml_lint/reporter.rb
@@ -1,16 +1,19 @@
 module HamlLint
-  # @abstract Abstract lint reporter. Subclass and override {#report_lints} to
-  #   implement a custom lint reporter.
+  # Abstract lint reporter. Subclass and override {#report_lints} to
+  # implement a custom lint reporter.
+  #
+  # @abstract
   class Reporter
     attr_reader :lints
 
     # @param logger [HamlLint::Logger]
-    # @param lints [Array<HamlLint::Lint>]
+    # @param report [HamlLint::Report]
     def initialize(logger, report)
       @log = logger
       @lints = report.lints
     end
 
+    # Implemented by subclasses to display lints from a {HamlLint::Report}.
     def report_lints
       raise NotImplementedError
     end
