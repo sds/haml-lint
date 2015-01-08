92b0295c2a529af7593519fce5695fcd6727527d
David Rodríguez <deivid.rodriguez@gmail.com>
2015-01-08 10:28:36 +0100

    Explicitly set test target pattern to current file
    In travis, bundled gems are located in the vendor folder in the current
    directory. The default pattern scans every haml file recursively in the
    current directory, so it finds offenses in bundled gems.
    
    I fix this by setting the pattern to just the current file, which has (and
    should always have) no offenses

--- spec/haml_lint/rake_task_spec.rb
+++ spec/haml_lint/rake_task_spec.rb
@@ -3,17 +3,17 @@ require 'haml_lint/rake_task'
 
 describe HamlLint::RakeTask do
   before do
-    # Silence console output
-    # STDOUT.stub(:write)
+    @task = HamlLint::RakeTask.new
   end
 
   describe '#run' do
     subject do
-      HamlLint::RakeTask.new
       Rake::Task['haml_lint']
     end
 
     it 'returns a successful exit code' do
+      @task.pattern = __FILE__
+
       expect(subject.invoke).to be_truthy
     end
   end
