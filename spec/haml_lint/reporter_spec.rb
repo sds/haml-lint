bf325b94c4ad9bc741bb2862c500cf6dcfac2277
Shane da Silva <shane@dasilva.io>
2015-06-23 19:39:06 -0700

    Add spec for Reporter base class

---
+++ spec/haml_lint/reporter_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe HamlLint::Reporter do
+  let(:reporter) { HamlLint::Reporter.new(double) }
+
+  describe '#display_report' do
+    subject { reporter.display_report(double) }
+
+    it 'raises an error' do
+      expect { subject }.to raise_error NotImplementedError
+    end
+  end
+end
