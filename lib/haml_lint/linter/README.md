ed0f552b1f0779a146901fd2cc00c6850a573fbe
Gareth Adams <gareth@Gareths-MacBook-Pro.local>
2016-07-05 20:17:55 +0100

    Allow configurable classes before IDs

--- lib/haml_lint/linter/README.md
+++ lib/haml_lint/linter/README.md
@@ -67,7 +67,9 @@ assigned.
 
 ## ClassesBeforeIds
 
-List classes before IDs in tags.
+Whether classes or ID attributes should be listed first in tags.
+
+### EnforcedStyle: 'class' (default)
 
 **Bad: ID before class**
 ```haml
@@ -82,6 +84,22 @@ List classes before IDs in tags.
 These attributes should be listed in order of their specificity. Since the tag
 name (if specified) always comes first and has the lowest specificity, classes
 and then IDs should follow.
+
+### EnforcedStyle: 'id'
+
+**Bad: Class before ID**
+```haml
+%tag.class#id
+```
+
+**Good**
+```haml
+%tag#id.class
+```
+
+As IDs are more significant than classes to the element they represent, IDs
+should be listed first and then classes should follow. This gives a more
+consistent vertical alignment of IDs.
 
 ## ConsecutiveComments
 
--- lib/haml_lint/linter/classes_before_ids.rb
+++ lib/haml_lint/linter/classes_before_ids.rb
@@ -13,13 +13,27 @@ module HamlLint
       # Convert ".class#id" into [.class, #id] (preserving order)
       components = node.static_attributes_source.scan(/[.#][^.#]+/)
 
+      first, second = attribute_prefix_order
+
       (1...components.count).each do |index|
-        next unless components[index].start_with?('.') &&
-                    components[index - 1].start_with?('#')
+        next unless components[index].start_with?(first) &&
+                    components[index - 1].start_with?(second)
 
         record_lint(node, 'Classes should be listed before IDs '\
                           "(#{components[index]} should precede #{components[index - 1]})")
         break
+      end
+    end
+
+    private
+
+    def attribute_prefix_order
+      enforced_style = config.fetch('EnforcedStyle', 'class')
+      case enforced_style
+      when 'id'
+        ['#', '.']
+      else
+        ['.', '#']
       end
     end
   end
