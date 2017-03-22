a14911e6c2d75ca8e25ffe6d179df93f6841dede
Joel Barker <joel@mojamail.com>
2017-03-22 09:07:03 -0700

    Fix wrong indentation lint message

--- lib/haml_lint/linter/indentation.rb
+++ lib/haml_lint/linter/indentation.rb
@@ -10,14 +10,17 @@ module HamlLint
     }.freeze
 
     def visit_root(root)
-      regex = INDENT_REGEX[config['character'].to_sym]
+      character = config['character'].to_sym
+      wrong_characters = character == :space ? 'tabs' : 'spaces'
+
+      regex = INDENT_REGEX[character]
       dummy_node = Struct.new(:line)
 
       document.source_lines.each_with_index do |line, index|
         next if line =~ regex
 
         unless root.node_for_line(index).disabled?(self)
-          record_lint dummy_node.new(index + 1), 'Line contains tabs in indentation'
+          record_lint dummy_node.new(index + 1), "Line contains #{wrong_characters} in indentation"
         end
       end
     end
