7d28900ea178c2787a4356b3de0489e66180c4a4
Michael Herold <michael.j.herold@gmail.com>
2017-03-23 19:05:24 -0500

    Fix bugs in `--auto-gen-config`
    Whoops, I unintentionally shipped something without a test and lo!, it
    broke.
    
    Also, I was paying too close attention to the RuboCop format for the
    configuration file and unintentionally made it output a malform
    haml-lint configuration.
    
    Closes #207
    Closes #208

--- spec/haml_lint/reporter/disabled_config_reporter_spec.rb
+++ spec/haml_lint/reporter/disabled_config_reporter_spec.rb
@@ -35,14 +35,14 @@ RSpec.describe HamlLint::Reporter::DisabledConfigReporter do
     end
 
     context 'when there are lints' do
-      let(:files)        { ['some-filename.haml', 'other-filename.haml'] }
-      let(:lines)        { [502, 724] }
-      let(:descriptions) { ['Description of lint 1', 'Description of lint 2'] }
+      let(:files)        { ['some-filename.haml', 'some-filename.haml', 'other-filename.haml'] }
+      let(:lines)        { [502, 504, 724] }
+      let(:descriptions) { Array.new(3) { |i| "Description of lint #{i + 1}" } }
       let(:header)       { output.split("\n")[0..3].join("\n") }
       let(:linter)       { double(name: 'SomeLinter') }
       let(:offenses)     { output_without_summary.split("\n")[1..-1].join("\n") }
       let(:output_without_summary) { output.split("\n").reject(&:empty?)[0..-2].join("\n") }
-      let(:severities)   { [:warning] * 2 }
+      let(:severities)   { [:warning] * 3 }
       let(:summary)      { output.split("\n")[-4..-1].join("\n") }
 
       let(:lints) do
@@ -53,7 +53,7 @@ RSpec.describe HamlLint::Reporter::DisabledConfigReporter do
 
       # This is a hack to get around the fact that the method isn't called in isolation
       before do
-        linters_with_lints = { linter.name => files }
+        linters_with_lints = { linter.name => files.uniq }
         linters_lint_count = { linter.name => lints.size }
         reporter.__send__(:instance_variable_set, :@linters_with_lints, linters_with_lints)
         reporter.__send__(:instance_variable_set, :@linters_lint_count, linters_lint_count)
@@ -66,12 +66,17 @@ RSpec.describe HamlLint::Reporter::DisabledConfigReporter do
 
       it 'prints each lint on its own line' do
         subject
-        offenses.split("\n").size.should == 4
+        offenses.should == \
+          "other-filename.haml:724 [W] SomeLinter: Description of lint 3\n" \
+          "some-filename.haml:502 [W] SomeLinter: Description of lint 1\n" \
+          "some-filename.haml:504 [W] SomeLinter: Description of lint 2\n" \
+          "3 files inspected, 3 lints detected\n" \
+          'Created .haml-lint_todo.yml.'
       end
 
       it 'prints the summary' do
         subject
-        summary.should == "\n2 files inspected, 2 lints detected\n" \
+        summary.should == "\n3 files inspected, 3 lints detected\n" \
           "Created .haml-lint_todo.yml.\n" \
           'Run `haml-lint --config .haml-lint_todo.yml`, or add '\
           '`inherits_from: .haml-lint_todo.yml` in a .haml-lint.yml file.'
@@ -83,12 +88,59 @@ RSpec.describe HamlLint::Reporter::DisabledConfigReporter do
           [
             described_class::HEADING,
             '',
-            '# Offense count: 2',
-            'SomeLinter:',
-            '  Exclude:',
-            '    - "some-filename.haml"',
-            '    - "other-filename.haml"'
+            'linters:',
+            '',
+            '  # Offense count: 3',
+            '  SomeLinter:',
+            '    exclude:',
+            '      - "some-filename.haml"',
+            '      - "other-filename.haml"'
           ].join("\n")
+      end
+    end
+  end
+
+  describe '#finished_file' do
+    subject { reporter.finished_file(file, lints) }
+
+    context 'when there are no lints' do
+      let(:file) { 'some-filename.haml' }
+      let(:lints) { [] }
+
+      it 'does not log any lints on linters' do
+        expect { subject }.not_to change(reporter, :linters_with_lints)
+      end
+
+      it 'does not log any lint counts' do
+        expect { subject }.not_to change(reporter, :linters_lint_count)
+      end
+    end
+
+    context 'when there are lints' do
+      let(:file) { 'some-filename.haml' }
+      let(:lines)        { [502, 504, 724] }
+      let(:descriptions) { Array.new(3) { |i| "Description of lint #{i + 1}" } }
+      let(:header)       { output.split("\n")[0..3].join("\n") }
+      let(:linter)       { double(name: 'SomeLinter') }
+      let(:offenses)     { output_without_summary.split("\n")[1..-1].join("\n") }
+      let(:output_without_summary) { output.split("\n").reject(&:empty?)[0..-2].join("\n") }
+      let(:severities)   { [:warning] * 3 }
+      let(:summary)      { output.split("\n")[-4..-1].join("\n") }
+
+      let(:lints) do
+        lines.each_with_index.map do |line, index|
+          HamlLint::Lint.new(linter, file, line, descriptions[index], severities[index])
+        end
+      end
+
+      it 'logs lints on the linters' do
+        expect { subject }.to change(reporter, :linters_with_lints).to(
+          'SomeLinter' => ['some-filename.haml']
+        )
+      end
+
+      it 'logs any lint counts' do
+        expect { subject }.to change(reporter, :linters_lint_count).to('SomeLinter' => 3)
       end
     end
   end
