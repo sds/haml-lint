e4cd4ebc748e2207e7bf097e0490c15ae6c6d692
Ana María Martínez Gómez <ammartinez@suse.de>
2017-03-24 09:39:04 +0100

    Fix duplicated entries in haml-lint_todo.yml
    When autogenerating `haml-lint_todo.yml` It seems that every file is added to
    the exclude section of the haml-lint_todo.yml as many times as offenses it has.
    
    Fixes #208

--- lib/haml_lint/reporter/disabled_config_reporter.rb
+++ lib/haml_lint/reporter/disabled_config_reporter.rb
@@ -65,7 +65,7 @@ module HamlLint
 
       if lints.any?
         lints.each do |lint|
-          linters_with_lints[lint.linter.name] << lint.filename
+          linters_with_lints[lint.linter.name] |= [lint.filename]
           linters_lint_count[lint.linter.name] += 1
         end
       end
