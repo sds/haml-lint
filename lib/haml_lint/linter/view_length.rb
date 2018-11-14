63a98da3bb492a33113bd7540ba6e4aa2dbf09d5
Adrian Heaney <adrian@bonkers.ie>
2018-11-14 16:25:31 +0000

    allow ViewLength linter be disabled inline

--- lib/haml_lint/linter/view_length.rb
+++ lib/haml_lint/linter/view_length.rb
@@ -7,11 +7,12 @@ module HamlLint
 
     DummyNode = Struct.new(:line)
 
-    def visit_root(_root)
+    def visit_root(root)
       max = config['max']
       line_count = document.source_lines.count
+      node = root.children.first
 
-      if line_count > max
+      if line_count > max && !node.disabled?(self)
         record_lint(DummyNode.new(0), format(MSG, line_count, max))
       end
     end
