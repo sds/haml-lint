sds/haml-lint

commit 30eb877922ddf636c198ce7efbfc17e1200f7e48

Fix RSpec test for Rake task on Ruby 2.6
The RSpec test was breaking due to a temporary file being deleted before
all tests had run, causing errors.

This is likely due to changes in the GC implementation, but I didn't
spend too much time digging in.

To work around, modify the `config` property of the `HamlLint::RakeTask`
class to support passing either a string (like before) OR a `File`.
Allowing a `File` object allows the reference to remain ensuring that
the file is not destroyed until all tests have completed.

lib/haml_lint/rake_task.rb
@@ -35,9 +35,18 @@ module HamlLint
     # @return [String]
     attr_accessor :name
 
-    # Configuration file to use.
+    # Set the configuration file or path.
+    #
+    # @param config [String,File]
+    def config=(config)
+      @config = config.is_a?(String) ? File.open(config, 'r') : config
+    end
+
+    # Return the configuration file path.
     # @return [String]
-    attr_accessor :config
+    def config
+      @config.path
+    end
 
     # List of files to lint (can contain shell globs).
     #

spec/haml_lint/rake_task_spec.rb
@@ -11,7 +11,7 @@ describe HamlLint::RakeTask do
       end
 
     HamlLint::RakeTask.new do |t|
-      t.config = config.path
+      t.config = config
       t.fail_level = 'warning'
       t.quiet = true
     end

commit 8edb6ae57f95be0474bea44725c7f3c6757ae925

Fix Rake task loading
This was broken in 30eb87792 because we didn't test the case when
`config` was not explicitly specified.

I tried coming up with a test case for this but the Rake ecosystem makes
this rather odd. You can only instantiate one instance of a given Rake
task (based on my understanding) and there isn't a convenient way to
swap this out since you can't utilize a `let` in a `before(:all)`
context.

For now, I manually verified this fix works locally and will leave it at
that for now.

Fixes #300

lib/haml_lint/rake_task.rb
@@ -47,7 +47,7 @@ module HamlLint
     # Return the configuration file path.
     # @return [String]
     def config
-      @config.path
+      @config&.path
     end
 
     # List of files to lint (can contain shell globs).