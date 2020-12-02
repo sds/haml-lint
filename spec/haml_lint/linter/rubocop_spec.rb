bd821862d89fff40d8864e738f5dbf93d68320ce
Jonathan del Strother <jdelStrother@gmail.com>
2020-12-02 13:14:29 +0000

    Fix Marshal.dump failing on Rubocop linter
    Having RuboCop::CLI stored as an ivar in the linter causes Marshal.dump
    to fail ("can't dump hash with default proc") when haml-lint was run
    with --parallel.

--- spec/haml_lint/linter/rubocop_spec.rb
+++ spec/haml_lint/linter/rubocop_spec.rb
@@ -14,7 +14,7 @@ describe HamlLint::Linter::RuboCop do
     # before linter is run
     before do
       rubocop_cli.stub(:run).and_return(::RuboCop::CLI::STATUS_SUCCESS)
-      ::RuboCop::CLI.stub(:new).and_return(rubocop_cli)
+      HamlLint::Linter::RuboCop.stub(:rubocop_cli).and_return(rubocop_cli)
       HamlLint::OffenseCollector.stub(:offenses)
                                 .and_return([offence].compact)
     end
