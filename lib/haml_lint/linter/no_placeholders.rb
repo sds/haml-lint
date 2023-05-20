cfe0ff014505832136ee2c50a1b01d20a5bd39e7
benmelz <ben@melz.me>
2023-05-20 17:46:45 -0400

    feat: no placeholders linter

---
+++ lib/haml_lint/linter/no_placeholders.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module HamlLint
+  # Checks that placeholder attributes are not used.
+  class Linter::NoPlaceholders < Linter
+    include LinterRegistry
+
+    MSG = 'Placeholders attributes should not be used.'
+    HASH_REGEXP = /:?['"]?placeholder['"]?(?::| *=>)/.freeze
+    HTML_REGEXP = /placeholder=/.freeze
+
+    def visit_tag(node)
+      return unless node.hash_attributes_source =~ HASH_REGEXP || node.html_attributes_source =~ HTML_REGEXP
+
+      record_lint(node, MSG)
+    end
+  end
+end
